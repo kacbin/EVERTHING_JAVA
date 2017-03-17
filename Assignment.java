a1b9bd8713943eca7565986118475533570b38ed
Binod Kachhepati <Binod Kachhepati>
2017-03-17 11:17:35 -0500

    Added multiple files

---
+++ Assignment.java
@@ -0,0 +1,13 @@
+class Assignment{
+public static void main(String[] args){
+System.out.println("-------------");
+System.out.println("|  @      @  |");
+System.out.println("      =       ");
+System.out.println("-------------");
+System.out.println("      |      ");
+System.out.println("     / \\     ");
+System.out.println("      |      ");
+System.out.println("     / \\     ");
+}
+}
+
---
+++ BankOfAmerica.java
@@ -0,0 +1,9 @@
+	
+public class BankOfAmerica extends Banking{
+	public static void main(String[] args) {
+		BankOfAmerica object=new BankOfAmerica();
+		object.deposit(50000);
+		object.withdraw(10000);
+	}
+
+}
